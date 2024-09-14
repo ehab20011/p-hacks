a754d280ae3924b19f2e1313d64aaf2592b60bab
Zian <hasanzian6@gmail.com>
2024-09-14 14:14:28 -0400

    merged sophia's code

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -6,6 +6,7 @@ import bgimg from "./images/login.jpg";
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [role, setRole] = useState("refugee");
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
@@ -18,7 +19,7 @@ function Login() {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ email, password, role }),
       });
 
       const data = await response.json();
@@ -34,11 +35,6 @@ function Login() {
     }
   };
 
-  const handleSignupClick = () => {
-    // Navigate to the signup worker page
-    navigate('/signup-refugee');
-  };
-
   return (
     <div className="main">
       <h2 className="refu">RefuConnect</h2>
@@ -47,34 +43,59 @@ function Login() {
           <img src={bgimg} alt="Background" />
         </div>
         <div className="right-container">
-          <div className="options">
-            <h2>Login</h2>
-            <h2 onClick={handleSignupClick} style={{ cursor: 'pointer' }}>Sign Up</h2>
-          </div>
-          <div>
-            <form onSubmit={handleLogin}>
-              <div className="user-input">
-                <input
-                  type="text"
-                  placeholder="Email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <input
-                  type="password"
-                  placeholder="Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-              {error && <p style={{ color: 'red' }}>{error}</p>}
-              <button type="button" className="forgot-password">
-                Forgot Password?
-              </button>
-              <button type="submit" className="btn-login">
-                Login
-              </button>
-            </form>
+          <div className="inner-right">
+            <div className="options">
+              <h2>Login</h2>
+              <h2>Sign Up</h2>
+            </div>
+            <div>
+              <form onSubmit={handleLogin}>
+                <div className="user-input">
+                  <input
+                    type="text"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <div className="choose-role">
+                  <label>
+                    <input
+                      type="radio"
+                      name="role"
+                      value="refugee"
+                      checked={role === "refugee"}
+                      onChange={(e) => setRole(e.target.value)}
+                    />
+                    Refugee
+                  </label>
+                  <br />
+                  <label>
+                    <input
+                      type="radio"
+                      name="role"
+                      value="employee"
+                      checked={role === "employee"}
+                      onChange={(e) => setRole(e.target.value)}
+                    />
+                    Employee
+                  </label>
+                </div>
+                {error && <p style={{ color: 'red' }}>{error}</p>}
+                <a href="#" className="forgot-password">
+                  Forgot Password?
+                </a>
+                <button type="submit" className="btn-login">
+                  Login
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
