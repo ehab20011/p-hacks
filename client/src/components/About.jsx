69dfd935c5b5d93a7c61a5db85bd25cff76ff112
sophiayau <124006972+sophiayau@users.noreply.github.com>
2024-09-14 22:03:03 -0400

    started About.jsx and css file, added to Login.jsx

---
+++ client/src/components/About.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import "./styles/About.css";
+
+function About() {
+  return (
+    <div>
+      <h4>
+        As of May 26th 2024, New York City alone had over 65,600 people seeking
+        asylum in encampments & shelters. Over 200,000 as well since just 2022.{" "}
+      </h4>
+    </div>
+  );
+}
+
+export default About;
